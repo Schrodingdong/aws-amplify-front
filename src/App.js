3bceba6382e8674a794e7ba82c3f5850d76fa7e7
Schrodingdong <hamzaalamiibnjamaa123456789@gmail.com>
2023-09-15 16:49:14 +0100

    Updated the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { FaWindowClose } from 'react-icons/fa'
+import axios from 'axios';
 
 function App() {
+  const [base, setBase] = useState(0);
+  const [exponent, setExponent] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setresult] = useState(0);
+  const gatewayURL = "https://zn6dl04ya9.execute-api.eu-west-1.amazonaws.com/default/math-function"
+
+
+
+  const doCalculation = (e) => {
+    e.preventDefault();
+    // send to lambda
+    axios.post(
+      gatewayURL,
+      {
+        base : base,
+        exponent: exponent
+      }
+    ).then( result => {
+      setresult(result.result);
+      setShowResult(true);
+    })
+
+
+
+  }
+
+  const closePopup = () => {
+    setShowResult(false);
+  }
+
+
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quick Mafs</h1>
+      <p>do some fast fast qalkulation : exponential !!</p>
+      <form className='math-form' onSubmit={doCalculation}>
+        <div>
+          <label htmlFor='base'>Base</label>
+          <input name="base" type='number' onChange={e => setBase(e.target.value)}/>
+        </div>
+        <div>
+          <label htmlFor='exponent'>Exponent</label>
+          <input name="exponent" type='number' onChange={e => setExponent(e.target.value)}/>
+        </div>
+        <input type='Submit' value={"Calculate"}/>
+      </form>
     </div>
+      {
+        showResult?
+          <Result result={result} closePopup={closePopup}/>
+          : <></>
+      }
+    </>
   );
+}
+
+
+const Result = ({result, closePopup}) => {
+  return (
+    <div className='result' onClick={closePopup}>
+      <div className='popup'>
+        {/* <FaWindowClose className='absolute top right' onClick={closePopup()}/> */}
+        <h1>Result</h1>
+        <h3>{result}</h3>
+      </div>
+    </div>
+  )
 }
 
 export default App;
